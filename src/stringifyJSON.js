be22e6f6eedde87ac48b44ca99a186862f5314ac
Tyler Davis <datadazer@gmail.com>
2014-11-18 14:45:54 -0800

    stringifyJSON now checks for value types numbers, booleans, strings, and arrays. However working on exceptions still.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,20 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
+  
+  if(obj != NaN && obj != null && typeof obj !== 'string' && Object.prototype.toString.call(obj) !== "[object Array]")
+    return obj.toString();
+
+  else if(typeof obj === 'string'){
+    return "\"" + obj + "\"";
+  }
+  else if(Object.prototype.toString.call(obj) === "[object Array]"){
+    return "[" +obj+ "]";
+  }
+  
+  else{
+   return String(obj); 
+  }
+
   // your code goes here
 };
