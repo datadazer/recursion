0e1bce1752bbafdf9c7e4cc16f78ea687be94d03
Tyler Davis <datadazer@gmail.com>
2014-11-18 16:50:00 -0800

    added comments to the code in progress.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,31 +5,30 @@
 var stringifyJSON = function(obj) {
   var result = [];
   var i = 0;
-  if(obj != NaN && obj != null && typeof obj !== 'string' && Object.prototype.toString.call(obj) !== "[object Array]")
+  if(obj != NaN && obj != null && typeof obj !== 'string' && Object.prototype.toString.call(obj) !== "[object Array]") //checks if able to perform .toString on obj
     return obj.toString();
 
-  else if(typeof obj === 'string'){
+  else if(typeof obj === 'string'){ //is obj a string?
     return "\"" + obj + "\"";
   }
-  else if(Object.prototype.toString.call(obj) === "[object Array]"){
-    if(obj.length > 1 && i < obj.length){
+  else if(Object.prototype.toString.call(obj) === "[object Array]"){ //is obj an array?
+    if(obj.length > 1 && i < obj.length){ //does obj have more than one element in its list?
       while(i < obj.length){
         result.push(stringifyJSON(obj[i]));
         i++;
       }
     }
-    else{
-      if(typeof obj[0] === 'string'){
+    else{//if it is an array with a length less than 1.
+      if(typeof obj[0] === 'string'){//is the element inside array a string?
         return "["+"\""+obj+"\""+"]";
       }
-      return "[" +obj+ "]";
+      return "[" +obj+ "]";//if not a string.
     }
-    return "["+result+"]";
+    return "["+result+"]"; // if obj was array.length > 1... return the while loop result from above.
   }
   
   else{
-   return String(obj); 
+   return String(obj); //if it isn't any of the above types.
   }
 
-  // your code goes here
 };
