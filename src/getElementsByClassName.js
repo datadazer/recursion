96d0a046c732f1eeb0b4ff05a3425ee87baae9f9
Tyler Davis <datadazer@gmail.com>
2014-11-22 20:28:49 -0800

    getElementsByClassName takes className and slices info from body and p elements and pushes them to result that returns.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,13 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+	var result = [];
+	var $body = $("body");
+	var $p = $("p");
+	var tempResult;
+	tempResult = $body.slice();
+	result.push(tempResult[0]);
+	tempResult = $p.slice();
+	result.push(tempResult[0]);
+	return result;
 };
