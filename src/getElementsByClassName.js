008bd35366b6201e4b8b4957a6e967eefa36c364
Tyler Davis <datadazer@gmail.com>
2014-11-26 04:53:01 -0800

    gEBCN removed extraneous if statement.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,13 +7,13 @@
 var getElementsByClassName = function(className){
 	var result = [];
 	var allNodes = document.body.childNodes;
-		if(document.body){
+		//if(document.body){
 			result.push(document.body);
 		for(var i=0; i < allNodes.length; i++){
 			if(allNodes[i].className){
 				result.push(allNodes[i])
 			}
-		}
+		//}
 	}
 	return result;
 };
